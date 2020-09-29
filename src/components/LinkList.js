ec3c82a00258c79d629819387570ee66d1c69c4d
keisukekashimura <kashimura@refcome.com>
2020-09-29 09:37:59 +0900

    add query

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import {Link} from './Link'
+import {Query} from 'react-apollo'
+import gql from 'graphql-tag'
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`
 
 export const LinkList = () => {
-  const linksToRender = [
-    {id: '1', description: 'Prisma turns your database into a GraphQL API 😎', url: 'https://www.prismagraphql.com'},
-    {id: '2', description: 'The best GraphQL client', url: 'https://www.apollographql.com/docs/react/'},
-  ]
-
   return (
-    <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
+    <Query query={FEED_QUERY} >
+      {({loading, error, data}) => {
+        if (loading) return <div>Fetching</div>
+        if (error) return <div>Error</div>
+
+        const linksToRender = data.feed.links
+
+        return linksToRender.map((link) => <Link key={link.id} link={link}/>)
+      }}
+    </Query>
   )
 }
