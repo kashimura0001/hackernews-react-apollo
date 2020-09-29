628c905e4f9c2844b08287b4ab4c9e0f70b6bbff
keisukekashimura <kashimura@refcome.com>
2020-09-29 23:35:56 +0900

    add create link

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {LinkList} from './LinkList'
+// import {LinkList} from './LinkList'
+import {CreateLink} from './CreateLink'
 
 export const App = () => {
-  return <LinkList />
+  return <CreateLink />
 }
